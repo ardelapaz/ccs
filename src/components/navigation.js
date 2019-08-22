fdc57d7817df7d40106b0e3752dd8a59c503c2c1
Andrew Delapaz <ardelapaz@yahoo.com>
2019-08-22 15:07:12 -0400

    Updated nav bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
-import Schedule from './schedule/schedule';
-import Logout from './logout';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
 
 class navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          modal: false
-        };
-    
-        this.toggle = this.toggle.bind(this);
-      }
-    toggle() {
-        this.setState(prevState => ({
-          modal: !prevState.modal
-        }));
-      }
-
     render() {
         return (
-                <div className = "navigation-header">
-
-                <a href = "/" className = "main-image"><img src='../../images/ccs.png' href = "/" alt="ccs" className = "nav-logo" id = "nav-logo"/></a>
-                <div className = "nav-background"></div>
-                <ul className = "nav-links">
-                          <li onClick = { (e) => this.toggle(e, 2)} className = 'nav-link-box'><a className = 'nav-link-header' href="/schedule">Schedule</a></li>
-                          <li onClick = { (e) => this.toggle(e, 3)} className = 'nav-link-box'><a className = 'nav-link-header' href="/standings">Standings</a></li>
-                          <li onClick = { (e) => this.toggle(e, 5)} className = 'nav-link-box'><a className = 'nav-link-header' href="/team">My Team</a></li>
-                          <li onClick = { (e) => this.toggle(e, 6)} className = 'nav-link-box'><a className = 'nav-link-header' href="/support">Support</a></li>
-                          <Logout />
-                </ul> 
-                </div>   
+            <div className = "navigation-header">
+                <div className = "nav-box">
+                    <a href = "/" className = "main-image">
+                        <img src='../../images/ccs.png' href = "/" alt="ccs" className = "nav-logo" id = "nav-logo"/>
+                    </a>
+                    <ul className = "nav-links">
+                        <li className = 'nav-link-box'><a className = 'nav-link-header' href="/schedule">Schedule</a></li>
+                        <li className = 'nav-link-box'><a className = 'nav-link-header' href="/standings">Standings</a></li>
+                        <li className = 'nav-link-box'><a className = 'nav-link-header' href="/team">My Team</a></li>
+                        <li className = 'nav-link-box'><a className = 'nav-link-header' href="/support">Support</a></li>
+                    </ul> 
+                </div>
+            </div>   
         )
     }
 }
